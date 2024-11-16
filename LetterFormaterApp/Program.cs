d2c35aae6b8421e31461364be6f364cbcc316c86
tikh <Evgeniy@NB_EVGENIY>
2024-11-17 01:41:17 +0700

    Конечная программа с модульными тестами

---
+++ LetterFormaterApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LetterFormater;
+
+namespace LetterFormaterApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var formatter = new SimlpeTextFormater();
+            try
+            {
+                string result = formatter.FormatLine("Hello World", 20);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+    }
+}
